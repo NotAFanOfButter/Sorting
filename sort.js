52f5485fb5c2a6bf770249aa7631e5507f60055b
Nerdling_S <nerdlings9@gmail.com>
2018-12-23 12:45:27 +0200

    sort() swap() getArr()

--- sort.js
+++ sort.js
@@ -1,3 +1,28 @@
 class BubbleSort {
+    constructor(arr) {
+        this.mixed = arr;
+        this.working = mixed.slice(0);
+        this.sorted = [];
+    }
 
+    sort() {
+        while(this.sorted.length < this.mixed.length) {
+            for(let i = 0; i < this.working.length-1; i++) {
+                if(this.working[i] > this.working[i+1]) {
+                    swap(this.working[i], this.working[i+1]);
+                }
+            }
+            this.sorted.unshift(this.working.pop());
+        }
+    }
+
+    swap(left, right) {
+        let temp = left;
+        left = right;
+        right = temp;
+    }
+
+    getArr() {
+        return this.working + this.sorted;
+    }
 }
