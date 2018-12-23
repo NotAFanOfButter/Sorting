5ecaf7539c9d2412953be2cef1441fa41f3f2245
Nerdling_S <nerdlings9@gmail.com>
2018-12-23 11:58:55 +0200

    Mixed Number Generation

--- sketch.js
+++ sketch.js
@@ -1,8 +1,31 @@
+const maxNum = 10;
 function setup() {
 	createCanvas(625, 625);
 	background(0);
+
+	let options = [];
+	for(let i = 0; i < maxNum; i++) {
+		options[i] = i;
+	}
+
+	console.log(mixNumbers(options));
 }
 
 function draw() {
 
+}
+
+const mixNumbers = function(options) {
+	let mixed = [];
+	const len = options.length
+	for(let i = 0; i < len; i++) {
+		mixed[i] = random(options);
+		for(let j = 0; j < options.length; j++) {
+			if(options[j] == mixed[i]) {
+				options.splice(j,1);
+			}
+		}
+	}
+
+	return mixed;
 }
