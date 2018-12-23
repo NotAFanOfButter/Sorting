89307c33b74bc26bd21ada30ed4428a5e7fe8e25
Nerdling_S <nerdlings9@gmail.com>
2018-12-23 12:16:04 +0200

    Visualization
    implemented visualization by way of a display function, which takes in
    an array and displays it as a series of array.length squares, in HSB (max
    value is array.length) with the hue set at the element's value.

--- sketch.js
+++ sketch.js
@@ -1,31 +1,46 @@
-const maxNum = 10;
+const maxNum = 4;
+
 function setup() {
 	createCanvas(625, 625);
 	background(0);
 
 	let options = [];
-	for(let i = 0; i < maxNum; i++) {
+	for (let i = 0; i < maxNum; i++) {
 		options[i] = i;
 	}
 
-	console.log(mixNumbers(options));
+	let mixed = mixNumbers(options);
+	display(mixed);
 }
 
 function draw() {
 
 }
 
-const mixNumbers = function(options) {
+const mixNumbers = function (options) {
 	let mixed = [];
 	const len = options.length
-	for(let i = 0; i < len; i++) {
+	for (let i = 0; i < len; i++) {
 		mixed[i] = random(options);
-		for(let j = 0; j < options.length; j++) {
-			if(options[j] == mixed[i]) {
-				options.splice(j,1);
+		for (let j = 0; j < options.length; j++) {
+			if (options[j] == mixed[i]) {
+				options.splice(j, 1);
 			}
 		}
 	}
 
 	return mixed;
+}
+const display = function (arr) {
+	let side = width/sqrt(maxNum);
+	let i = 0;
+	for(let row = 0; row < height/side; row++) {
+		for(let col = 0; col < width/side; col++) {
+			colorMode(HSB, maxNum)
+			fill(arr[i], maxNum, maxNum);
+			noStroke();
+			rect(col*side, row*side, side, side);
+			i++;
+		}
+	}
 }
